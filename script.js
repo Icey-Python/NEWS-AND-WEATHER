964f334d059fea3cd663f3c977e0466790c1c501
Icey-Python <65103243+Icey-Python@users.noreply.github.com>
2023-09-13 09:36:36 +0000

    checking articles obtained

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Add CORS proxy url
-const corsProxy = 'https://cors-anywhere.herokuapp.com/'; 
-const news_url = corsProxy  + "https://news-feed-ke.vercel.app/news"
+const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+const news_url = corsProxy + "https://news-feed-ke.vercel.app/news"
 
 // DOM Elements
 const newsArticles = document.getElementById('news-articles');
@@ -10,20 +10,20 @@ const categoryLinks = document.querySelectorAll('.category');
 
 // Get News Articles
 function getNews() {
-	$.ajax({
-		url: news_url,
-	}).done(function(response) {
-    console.log(response)
-		let articles = response.data;
-		let output = '';
+  $.ajax({
+    url: news_url,
+  }).done(function(response) {
+    let articles = response.data;
+    console.log(articles)
+    let output = '';
 
-		articles.forEach(function(article) {
+    articles.forEach(function(article) {
       content = ""
-      (article.content).forEach((paragraph)=>{
-        content += paragraph
-      })
-            output +=`<article>`
-            output += `
+        (article.content).forEach((paragraph) => {
+          content += paragraph
+        })
+      output += `<article>`
+      output += `
 					<h3>${article.title}</h3>
 					<img src='${article.image}'/>
 					<h6>${article.author}</h6>
@@ -32,11 +32,11 @@ function getNews() {
 				</article>
 			`;
 
-		});
-		newsArticles.innerHTML = output;
-	}).fail(function(error) {
-		console.log(error);
-	});
+    });
+    newsArticles.innerHTML = output;
+  }).fail(function(error) {
+    console.log(error);
+  });
 }
 getNews();
 
