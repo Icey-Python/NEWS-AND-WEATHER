8e785e8453f28151f481dc5dc8451204e0eaa1a0
Icey Python <samkellyndungu@gmail.com>
2023-09-17 00:01:37 +0300

    updated ui to category access

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const newsArticles = document.getElementById('news-articles');
 const header_tag = document.querySelector('header')
 const load_more = document.querySelector('.button')
 const loader = document.querySelector('.loader')
+const category_tab = document.querySelector('.category-holder')
 
 let weatherUrl = `https://news-feed-ke.vercel.app/proxy_weather`
 let current_page = 0
@@ -24,6 +25,32 @@ load_more.addEventListener('click',()=>{
   current_page = (current_page + 1)%page_count
   getNews()
 })
+function getCategory(category){
+  loader.classList.remove('hidden')
+  if (category == "Life & Style"){
+    category = "Life%20%26%20Style"
+  }
+  $.ajax({
+    url:`https://news-feed-ke.vercel.app/news/category/${category}`
+  }).done((data)=>{
+    loader.classList.add('hidden')
+    let output = '';
+    data.content.forEach((article)=>{
+      output += `<article>`
+      output += `
+        <h2 class='bounded' >${article.title}</h2>
+        <img src='${proxy}=${article.image_url}'/>
+        <h6>${article.author}| ${article.category}</h6>
+        <h4><em>${article.image_description}</em></h4>
+        <p>${article.content}</p>
+        <em class='date'>${article.date}</em>
+      </article>
+    `;
+
+    })
+    newsArticles.innerHTML = output
+  })
+}
 // Get News Articles
 function getNews() {
   $.ajax({
@@ -32,6 +59,20 @@ function getNews() {
     loader.classList.add('hidden')
     newsArticles.classList.remove('hidden')
     load_more.classList.remove('hidden')
+    $.ajax({
+      url: `${news_url}/categories`
+    }).done((data)=>{
+      let category_chips = ''
+      data.forEach((category)=>{
+        console.log(category)
+        category_chips +=`
+        <button class="category" onclick=getCategory('${encodeURIComponent(category)}')>
+					<span class="lable">${category}</span>
+        </button>
+        `
+      })
+      category_tab.innerHTML = category_chips
+    })
     let articles = response.content;
     page_count = response.page_count + 1
     let output = '';
