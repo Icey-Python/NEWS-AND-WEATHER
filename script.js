8d166d1d685c143783d15c628e87f7cb8c3fdccc
Icey-Python <ndungusamkelly5@gmail.com>
2024-01-20 01:22:20 +0300

    Changed UI

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ load_more.addEventListener('click',()=>{
   current_page = (current_page + 1)%page_count
   getNews()
 })
+
+var article_data
 function getCategory(category){
   loader.classList.remove('hidden')
  
@@ -27,14 +29,13 @@ function getCategory(category){
   }).done((data)=>{
     loader.classList.add('hidden')
     let output = '';
-    data.content.forEach((article)=>{
-      output += `<article>`
+    data.content.forEach((article,idx)=>{
+      output += `<article id=${idx}>`
       output += `
         <h2 class='bounded' >${article.title}</h2>
         <img src='${proxy}=${article.image_url}'/>
         <h6>${article.author}| ${article.category}</h6>
-        <h4><em>${article.image_description}</em></h4>
-        <p>${article.content}</p>
+        <h4><em>${article.image_description}</em></h4> 
         <em class='date'>${article.date}</em>
       </article>
     `;
@@ -65,18 +66,25 @@ function getNews() {
       category_tab.innerHTML = category_chips
     })
     let articles = response.content;
+    article_data = articles
     page_count = response.page_count + 1
     let output = '';
 
-    articles.forEach(function(article) {
-      output += `<article>`
+    articles.forEach((article,idx)=>{
+      output += `<article id=${idx}>`
       output += `
         <h2 class='bounded' >${article.title}</h2>
         <img src='${proxy}=${article.image_url}'/>
         <h6>${article.author}| ${article.category}</h6>
         <h4><em>${article.image_description}</em></h4>
-        <p>${article.content}</p>
         <em class='date'>${article.date}</em>
+        <button onclick=show_news_in_popup(${idx})>
+    <span>read</span>
+    <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="37" cy="37" r="35.5" stroke="black" stroke-width="3"></circle>
+        <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="black"></path>
+    </svg>
+</button>
       </article>
     `;
 
@@ -85,6 +93,26 @@ function getNews() {
   }).fail(function(error) {
     console.log(error);
   });
+}
+
+
+function show_news_in_popup(id){
+  let popup =document.querySelector('.popup')
+  popup.addEventListener('dblclick',hide_popup)
+  popup.addEventListener('click',hide_popup)
+  let popup_inner = document.querySelector('.popup-inner')
+  let working_article = article_data[id]
+  popup_inner.innerHTML = `
+  <img src='${working_article.image_url}'/>
+  <h2>${working_article.title}</h2>
+  <p>${working_article.content}</p>
+  `
+  popup.classList.toggle('show-popup')
+}
+
+function hide_popup(){
+  let popup =document.querySelector('.popup')
+  popup.classList.toggle('show-popup')
 }
 
 async function getWeather() {
